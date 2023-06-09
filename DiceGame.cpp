8366144775c360e4d92161ef2e4b4f75a2bae298
MORI <105579842+hibiki333155555@users.noreply.github.com>
2023-06-09 11:31:38 +0900

    Won/Lost message added

--- DiceGame.cpp
+++ DiceGame.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 #define rep(i,a,b) for(int i=a;i<b;i++)
 
@@ -7,6 +8,7 @@ int main() {
     int n;
     n = 2;
     int ans = 0;
+    srand((unsigned int)time(NULL));
 
     rep(i, 1, n+1) {
         int x; x = rand()%6 + 1;
@@ -15,5 +17,7 @@ int main() {
     }
 
     cout << "Total value: " << ans;
+    if(ans > 7) cout << "You Won!" << endl;
+    else cout << "You Lost" << endl;
     return 0;
 }
